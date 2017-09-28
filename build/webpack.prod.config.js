54079f9c2c269fa75c0d6bbb6ac230cea8ba5147
zhangzc <mrzzhichao@gmail.com>
2017-09-28 17:01:12 +0800

    update code

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,15 +1,14 @@
 /**
  * Created by MChao on 2017/9/10.
  */
-const path = require('path')
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')   // 打包成一个css文件
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const base = require('./webpack.base.config.js')
 
 module.exports = merge(base, {
 	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
@@ -23,7 +22,22 @@ module.exports = merge(base, {
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vue',
             filename: 'vue.js'
-        })
+        }),
+		new CopyWebpackPlugin([
+			// {output}/to/file.txt
+			{ from: '../public/*', to: '../dist/' }],
+			 {
+				 ignore: [
+				// Doesn't copy any files with a txt extension
+				'*.html',
+				{ glob: '**/*', dot: false }
+			],
+
+			// By default, we only copy modified files during
+			// a watch or webpack-dev-server build. Setting this
+			// to `true` copies all files.
+			copyUnmodified: true
+		})
 	],
 	module: {
 		rules:
